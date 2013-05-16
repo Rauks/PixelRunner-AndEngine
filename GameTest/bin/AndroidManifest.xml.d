0c7f9b34eb22b6def5314d68b9c160ae4b9d5cb7
Karl Woditsch <k.woditsch@gmail.com>
2013-05-16 13:05:20 +0200

    Remove game test

--- GameTest/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\bin\AndroidManifest.xml : \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\AndroidManifest.xml \
--- GameTest/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\gen\org\gametest\R.java \
- : D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\res\drawable-hdpi\ic_launcher.png \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\res\drawable-ldpi\ic_launcher.png \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\res\drawable-mdpi\ic_launcher.png \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\res\values\strings.xml \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\bin\res\drawable-hdpi\ic_launcher.png \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\bin\res\drawable-ldpi\ic_launcher.png \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\bin\res\drawable-mdpi\ic_launcher.png \
-D:\Users\Karl\Documents\GitHub\AndEngine-GLES2-AnchorCenter-SandBox\GameTest\bin\AndroidManifest.xml \
